d79869725f30a9f604e255aa89645f713a5f5f1b
ShutianWang <77990268+ShutianWang@users.noreply.github.com>
2021-04-09 22:42:24 -0400

    js

--- public/scripts.js
+++ public/scripts.js
@@ -11,6 +11,87 @@ async function diningHall(){
         <th>${dhll.hall_address}</th>`
         diningTable.append(diningLocation);
     });
-}
+
+    const marcosRequest = await fetch('api/mealmacros');
+    const marcosData = await marcosRequest.json();
+    const chart = new CanvasJS.Chart('container', {
+        animationEnabled: true,
+        title: {
+            text:'Top Ten Mactos Meals'
+        },
+        axisX: {
+            interval: 1,
+            label: "Meals",
+        },
+        axisY: {
+            label: "Macros",
+        },
+        toolTip: {
+            shared: true
+        },
+        legend:{
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+                type: 'stackedBar',
+                name: 'Calories',
+                showInLegend: 'true',
+                dataPoints: caloriesData
+            },
+            {
+                type: 'stakedBar',
+                name: 'Cholesterol',
+                showInLenged: 'true',
+                dataPoints: cholesterolData
+            },
+            {
+                type: 'stakedBar',
+                name: 'Sodium',
+                showInLegend: 'true',
+                dataPoints: sodiumData
+            },
+            {
+                type: 'stakedBar',
+                name: 'Carbs',
+                showInLegend: 'true',
+                dataPoints: carbsData
+            },
+            {
+                type: 'stakedBar',
+                name: 'Protein',
+                showInLegend: 'true',
+                dataPoints: proteinData
+            },
+            {
+                type: 'stakedBar',
+                name: 'Fat',
+                showInLegend: 'true',
+                dataPoints: fatData
+            },
+        ]
+    });
+    
+    marcosData.forEach((meal) => {
+        caloriesData.push({label: meal.meal_name, y: meal.calories});
+        servingData.push({label: meal.meal_name, y: meal.serving_size});
+        cholData.push({label: meal.meal_name, y: meal.cholesterol});
+        sodiumData.push({label: meal.meal_name, y: meal.sodium});
+        carbsData.push({label: meal.meal_name, y: meal.carbs});
+        proteinData.push({label: meal.meal_name, y: meal.protein});
+        fatData.push({label: meal.meal_name, y: meal.fat});
+    });
+
+    chart.render();
+
+    function toggleDataSeries(e) {
+        if (typeof e.dataSeries.visible === 'undefined' || e.dataSeries.visible) {
+          e.dataSeries.visible = false;
+        } else {
+          e.dataSeries.visible = true;
+        }
+        chart.render();
+      }
+    }
 
 window.onload = diningHall();
